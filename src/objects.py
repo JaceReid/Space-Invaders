f90d8f3e9b4d96b7287d15ab4fc8fc9be46f912a
ralf0311 <126684702+ralf0311@users.noreply.github.com>
2023-04-23 13:09:57 +0200

    Update objects.py

--- src/objects.py
+++ src/objects.py
@@ -67,7 +67,7 @@ class enemy:
     def move(self, direction):
         move(self, direction, self.speed)
 
-    #
+    #creating a string consisting of the x and y positions and the rectangle
     def __str__(self):
         return str(self.x) + " " + str(self.y) + ";  " + str(self.r)
 
