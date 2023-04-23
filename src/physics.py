1fffb537f7ab5a82e9724d29305868fde36ef2a7
ralf0311 <126684702+ralf0311@users.noreply.github.com>
2023-04-23 12:48:22 +0200

    Update physics.py

--- src/physics.py
+++ src/physics.py
@@ -1,5 +1,6 @@
 import math
 
+#collisions function that detects when the x and y values of two objects are within a certain range of each other and returning the boolean x (True when within range)
 def colision(a,b,d):
     x = False
     if(abs(a.x - b.x) < d and abs(a.y - b.y) < d ):
